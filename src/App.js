7c186dd653e0147c3f3e1d7b08ab06f964fa27d1
GMalaconi <gmalaconi@protonmail.ch>
2022-02-08 20:51:01 -0300

    imported app from codepen profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: ""
+    }
+  }
+  
+  async getQuote() {
+    const response = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+    const data = await response.json();
+    let randomNum = Math.floor(Math.random() * data.quotes.length);
+    let randomQuote = data.quotes[randomNum];
+    
+    this.setState({
+      quote: randomQuote.quote,
+      author: randomQuote.author
+    });
+  }
+  
+  componentDidMount() {
+    this.getQuote();
+  }
+  
+  newQuote() {
+    this.getQuote();
+  }
+  
+  render() {
+    let twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quote}`;
+    return (
+      <div id="quote-box">
+        <div id="text">{this.state.quote}</div>
+        <div id="author">-{this.state.author}</div>
+        <button id="new-quote" onClick={this.newQuote.bind(this)}>new quote</button>
+        <a id="tweet-quote" target="_top" href={twitterUrl}>tweet it</a>
+      </div>
+    )
+  }
 }
 
 export default App;
